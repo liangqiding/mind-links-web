0ffb4d74df6af1c9392fcc29fc4440962ebf6ea4
liangqiding <742740345@qq.com>
2022-03-20 13:59:29 +0800

    重新初始化项目

---
+++ src/views/deviceManage/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <h1>设备管理</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
